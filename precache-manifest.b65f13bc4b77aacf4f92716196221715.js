d5a83fab1f0beb93f135f86d904716622d268a80
mehmet yagci <mehmetyagci53@hotmail.com>
2020-01-03 16:38:07 +0300

    Updates

---
+++ precache-manifest.b65f13bc4b77aacf4f92716196221715.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "51b5f7fc55db6ebdcb2acd55a36a3792",
+    "url": "/MyReads/index.html"
+  },
+  {
+    "revision": "dd62e95e7746f6b03ca9",
+    "url": "/MyReads/static/css/main.841dd37c.chunk.css"
+  },
+  {
+    "revision": "bbeb7bfea17c8d279b73",
+    "url": "/MyReads/static/js/2.2adb645b.chunk.js"
+  },
+  {
+    "revision": "dd62e95e7746f6b03ca9",
+    "url": "/MyReads/static/js/main.e1fc71ec.chunk.js"
+  },
+  {
+    "revision": "d31a376034036693ba9f",
+    "url": "/MyReads/static/js/runtime~main.55bd6041.js"
+  },
+  {
+    "revision": "bb9363b86b02777d23523851826414e0",
+    "url": "/MyReads/static/media/add.bb9363b8.svg"
+  },
+  {
+    "revision": "3687aa988bec28f17ae39153db1646c3",
+    "url": "/MyReads/static/media/arrow-back.3687aa98.svg"
+  },
+  {
+    "revision": "78c6f53b2342643639fe1cbaf171bf47",
+    "url": "/MyReads/static/media/arrow-drop-down.78c6f53b.svg"
+  }
+]);
